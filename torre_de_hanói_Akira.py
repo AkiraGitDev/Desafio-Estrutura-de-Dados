5f0a76df4542a3381738b815cddfd717fcdd182b
Thiago Akira <139590126+AkiraGitDev@users.noreply.github.com>
2024-08-19 17:16:30 -0300

    Update torre_de_hanói_Akira.py

--- torre_de_hanói_Akira.py
+++ torre_de_hanói_Akira.py
@@ -1,5 +1,7 @@
 """Torres de Hanói. O problema consiste em mover n discos empilhados (os menores sobre os maiores), de uma haste de origem (A), para uma haste de destino (C), na mesma ordem, respeitando as seguintes regras: apenas um disco pode ser movido por vez, não colocar um disco maior sobre um menor e poder usar uma haste auxiliar (B). """
+import time
 
+print("=== TORRE DE HANÓI ===")
 # Quantidade de discos
 n = int(input("Insira o número de discos: "))
 
@@ -12,8 +14,8 @@ movimentos = []
 pilha = [(n, A, C, B)]
 
 while True:
-    if not pilha:  # Verifica se a pilha está vazia
-        break  # Sai do loop se a pilha estiver vazia
+    if not pilha:
+        break
     
     discos, origem, destino, auxiliar = pilha.pop()
 
@@ -27,9 +29,21 @@ while True:
         pilha.append((1, origem, destino, auxiliar))
         pilha.append((discos - 1, origem, auxiliar, destino))
 
-# Exibindo os movimentos
-for movimento in movimentos:
-    disco, origem, destino = movimento
-    origem_nome = ['A', 'B', 'C'][[A, B, C].index(origem)]
-    destino_nome = ['A', 'B', 'C'][[A, B, C].index(destino)]
+for disco, origem, destino in movimentos:
+    if origem is A:
+        origem_nome = 'A'
+    elif origem is B:
+        origem_nome = 'B'
+    else:
+        origem_nome = 'C'
+
+    if destino is A:
+        destino_nome = 'A'
+    elif destino is B:
+        destino_nome = 'B'
+    else:
+        destino_nome = 'C'
+
+    time.sleep(0.5)
     print(f"Move o disco {disco} de {origem_nome} para {destino_nome}")
+print("===== FIM =====")
